f422471e42a84d4b554a242ff8c37aa16ac86ee7
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 14:54:40 +0200

    correction ajout HTML

--- exercice1.5.php
+++ exercice1.5.php
@@ -1,6 +1,20 @@
-<?php
-$int = NULL;
-echo $int;
-$int = 5;
-echo $int;
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1.5</title>
+</head>
+<body>
+    <h1>Exercice 5 Partie 1 : Les variables PHP</h1>
+    <p>
+        <?php
+            $int = NULL;
+            echo $int;
+            $int = 5;
+            echo $int;
+        ?>
+    </p>
+</body>
+</html>
